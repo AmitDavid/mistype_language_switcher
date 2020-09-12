a87d845bb7998d323034bdff4f6b708e1664e3eb
amit-david <amit.david@mail.huji.ac.il>
2020-09-12 09:14:04 +0300

    initial commit

---
+++ js/switcher.js
@@ -0,0 +1,8 @@
+document.body.style.border = "5px solid yellow";
+
+// var focused = document.activeElement;
+// console.log("Hello world!");
+//
+// if (focused.tagName == 'INPUT') {
+//     document.getElementById(document.activeElement.id).value = "My value";
+// }
