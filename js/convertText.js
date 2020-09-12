f222b5e4b5576dd78042640e2f0ba4ee43c8a5bc
amit-david <amit.david556@gmail.com>
2020-09-12 10:58:39 +0300

    implement converter

--- js/convertText.js
+++ js/convertText.js
@@ -5,22 +5,22 @@ dict_eng_to_heb = {
     'q': '/', 'w':'\'', 'e': 'ק', 'r': 'ר', 't': 'א', 'y': 'ט', 'u': 'ו', 'i': 'ן', 'o': 'ם', 'p': 'פ',
     'a': 'ש', 's': 'ד', 'd': 'ג', 'f': 'כ', 'g': 'ע', 'h': 'י', 'j': 'ח', 'k': 'ל', 'l': 'ך', ';':'ף',  '\'': ',',
     'z': 'ז', 'x': 'ס', 'c':'ב', 'v':'ה', 'b':'נ', 'n':'מ', 'm':'צ', ',':'ת', '.':'ץ', '/':'.'
-}
+};
 
 dict_heb_to_eng = {
     '/': 'q', '\'':'w', 'ק': 'e', 'ר': 'r', 'א': 't', 'ט': 'y', 'ו': 'u', 'ן': 'i', 'ם': 'o', 'פ': 'p',
     'ש': 'a', 'ד': 's', 'ג': 'd', 'כ': 'f', 'ע': 'g', 'י': 'h', 'ח': 'j', 'ל': 'k', 'ך': 'l', 'פ':';', ',': '\'',
-    'ז': 'z', 'ס': 'x', 'ב':'c', 'ה':'v', 'נ':'b', 'מ':'n', 'צ':'m', 'ת':',', 'ץ':'.', '.':'/',
-}
+    'ז': 'z', 'ס': 'x', 'ב':'c', 'ה':'v', 'נ':'b', 'מ':'n', 'צ':'m', 'ת':',', 'ץ':'.', '.':'/'
+};
+
+selcted_dict = dict_heb_to_eng;
 
 for (let ch of text) {
-    if (lay[ch] === undefined) {
+    if (selcted_dict[ch] === undefined) {
         convertedText += ch;
     } else {
-        convertedText += lay[ch];
+        convertedText += selcted_dict[ch];
     }
-
 }
 
-document.activeElement.value = convertedText
-
+document.activeElement.value = convertedText;
