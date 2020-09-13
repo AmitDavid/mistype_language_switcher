a83d4381b542de839bd5bd39a996b105dcd5fd72
amit-david <amit.david556@gmail.com>
2020-09-13 11:01:36 +0300

    hebrew <==> english works.

--- js/contextMenu.js
+++ js/contextMenu.js
@@ -6,14 +6,13 @@ function onCreated() {
   }
 }
 
+// Create menu button
 browser.menus.create({
   id: "switch-texts",
   title: "Switch Texts",
   contexts: ["editable"]
 }, onCreated);
 
-const convert_code = '';
-
 browser.menus.onClicked.addListener((info, tab) => {
-    browser.tabs.executeScript(tab.id, {file: "js/convertText.js"})
+  browser.tabs.executeScript(tab.id, {file: "js/convertText.js"})
 });
--- js/convertText.js
+++ js/convertText.js
@@ -1,26 +1,51 @@
-var text = document.activeElement.value.toLowerCase();
-var convertedText = "";
-
-dict_eng_to_heb = {
-    'q': '/', 'w':'\'', 'e': 'ק', 'r': 'ר', 't': 'א', 'y': 'ט', 'u': 'ו', 'i': 'ן', 'o': 'ם', 'p': 'פ',
-    'a': 'ש', 's': 'ד', 'd': 'ג', 'f': 'כ', 'g': 'ע', 'h': 'י', 'j': 'ח', 'k': 'ל', 'l': 'ך', ';':'ף',  '\'': ',',
-    'z': 'ז', 'x': 'ס', 'c':'ב', 'v':'ה', 'b':'נ', 'n':'מ', 'm':'צ', ',':'ת', '.':'ץ', '/':'.'
-};
-
-dict_heb_to_eng = {
-    '/': 'q', '\'':'w', 'ק': 'e', 'ר': 'r', 'א': 't', 'ט': 'y', 'ו': 'u', 'ן': 'i', 'ם': 'o', 'פ': 'p',
-    'ש': 'a', 'ד': 's', 'ג': 'd', 'כ': 'f', 'ע': 'g', 'י': 'h', 'ח': 'j', 'ל': 'k', 'ך': 'l', 'פ':';', ',': '\'',
-    'ז': 'z', 'ס': 'x', 'ב':'c', 'ה':'v', 'נ':'b', 'מ':'n', 'צ':'m', 'ת':',', 'ץ':'.', '.':'/'
-};
-
-selcted_dict = dict_heb_to_eng;
-
-for (let ch of text) {
-    if (selcted_dict[ch] === undefined) {
-        convertedText += ch;
-    } else {
-        convertedText += selcted_dict[ch];
-    }
+function is_english_char(ch) {
+    return "abcdefghijklmnoprstuwxyz".includes(ch);
 }
 
-document.activeElement.value = convertedText;
+function is_hebrew_char(ch) {
+    return "אבגדהוזחטיכךלמםנןסעפףצץקרשת".includes(ch);
+}
+
+function convert_text() {
+    // Get text in text-box
+    let text = document.activeElement.value.toLowerCase();
+    let convertedText = "";
+    let selected_dict;
+
+    const dict_eng_to_heb = {
+        'q': '/', 'w': '\'', 'e': 'ק', 'r': 'ר', 't': 'א', 'y': 'ט', 'u': 'ו', 'i': 'ן', 'o': 'ם', 'p': 'פ',
+        'a': 'ש', 's': 'ד', 'd': 'ג', 'f': 'כ', 'g': 'ע', 'h': 'י', 'j': 'ח', 'k': 'ל', 'l': 'ך', ';': 'ף', '\'': ',',
+        'z': 'ז', 'x': 'ס', 'c': 'ב', 'v': 'ה', 'b': 'נ', 'n': 'מ', 'm': 'צ', ',': 'ת', '.': 'ץ', '/': '.'
+    };
+
+    const dict_heb_to_eng = {
+        '/': 'q', '\'': 'w', 'ק': 'e', 'ר': 'r', 'א': 't', 'ט': 'y', 'ו': 'u', 'ן': 'i', 'ם': 'o', 'פ': 'p',
+        'ש': 'a', 'ד': 's', 'ג': 'd', 'כ': 'f', 'ע': 'g', 'י': 'h', 'ח': 'j', 'ל': 'k', 'ך': 'l', 'ף': ';', ',': '\'',
+        'ז': 'z', 'ס': 'x', 'ב': 'c', 'ה': 'v', 'נ': 'b', 'מ': 'n', 'צ': 'm', 'ת': ',', 'ץ': '.', '.': '/'
+    };
+
+// Find what is the written language
+    for (let ch of text) {
+        if (is_english_char(ch)) {
+            selected_dict = dict_eng_to_heb;
+            break;
+        } else if (is_hebrew_char(ch)) {
+            selected_dict = dict_heb_to_eng;
+            break;
+        }
+    }
+
+// Convert each character to its counterpart
+    for (let ch of text) {
+        if (selected_dict[ch] === undefined) {
+            convertedText += ch;
+        } else {
+            convertedText += selected_dict[ch];
+        }
+    }
+
+// Replace text in text-box
+    document.activeElement.value = convertedText;
+}
+
+convert_text();
